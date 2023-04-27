cf226896c31251c61a8972975fccb8dd00afee5d
Undefined <undefined@undefined.io>
2023-04-27 12:45:09 +0900

    Initial Commit

---
+++ Lab_AVPlayer_livestream/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_AVPlayer_livestream
+//
+//  Created by Samuel on 2023/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
